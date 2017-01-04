a6adc44972c582aec9cdfc32341a13606dfa707e
Tobias <Tobias@TobiasAH.clients.wireless.dtu.dk>
2017-01-04 12:55:37 +0100

    Added additional rent values for later use
    Necessary for implementing house building

--- src/entity/Street.java
+++ src/entity/Street.java
@@ -4,17 +4,28 @@ import java.awt.Color;
 
 public class Street extends Ownable {
 
-	private int rent;
+	private final int RENT_0;
+	private final int RENT_1;
+	private final int RENT_2;
+	private final int RENT_3;
+	private final int RENT_4;
+	private final int RENT_HOTEL;
 	
-	public Street(int id, Color color, int price, int rent) {
+	
+	public Street(int id, Color color, int price, int[] rents) {
 		super(id, color, price);
 
-		this.rent = rent;
+		this.RENT_0 = rents[0];
+		this.RENT_1 = rents[1];
+		this.RENT_2 = rents[2];
+		this.RENT_3 = rents[3];
+		this.RENT_4 = rents[4];
+		this.RENT_HOTEL = rents[5];
 	}
 
 	@Override
 	public int getRent() {
-		return rent;
+		return RENT_0;
 	}
 
 }
