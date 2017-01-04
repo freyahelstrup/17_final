68114adc0f243dce0363c4c6e1bb2d3c4e9ed849
Tobias <Tobias@TobiasAH.clients.wireless.dtu.dk>
2017-01-04 13:17:50 +0100

    Updated board to match Street controller + new Chance class

---
+++ src/entity/Chance.java
@@ -0,0 +1,18 @@
+package entity;
+
+import java.awt.Color;
+
+public class Chance extends Field {
+
+	public Chance(int id, Color color) {
+		super(id, color);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void landOnField(Player player) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
