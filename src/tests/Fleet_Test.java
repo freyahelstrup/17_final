ba3ec592c1e2b9fa64c9569b257259e1da961919
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-01-09 13:22:45 +0100

    Fleet test done, no problems

--- src/tests/Fleet_Test.java
+++ src/tests/Fleet_Test.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 
 import entity.Account;
 import entity.Fleet;
+import entity.Ownable;
+import entity.Field;
 import entity.Messages;
 import entity.Piece;
 import entity.Player;
@@ -24,10 +26,34 @@ public class Fleet_Test {
 			dataFields[i]=new Fleet(i+1, Color.white, 200);
 		}
 		
+		//Both of the following commands are necessary for making a player an owner of a field
 		player.getAccount().setOwnedField(dataFields[0]);
+		dataFields[0].setOwner(player);
+		
+		//Test 1 owned fleet
+		//System.out.println(dataFields[0].getRentsArray()[0]);
 		assertEquals(25, dataFields[0].getRent());
-		//Test id
-		//assertEquals(6, testField.getId());
+		
+		//Test 2 owned fleets
+		player.getAccount().setOwnedField(dataFields[1]);
+		dataFields[1].setOwner(player);
+		
+		//System.out.println(dataFields[1].getRentsArray()[1]);
+		assertEquals(50, dataFields[0].getRent());
+		
+		//Test 3 owned fleets
+		player.getAccount().setOwnedField(dataFields[2]);
+		dataFields[2].setOwner(player);
+		
+		//System.out.println(dataFields[2].getRentsArray()[2]);
+		assertEquals(100, dataFields[0].getRent());
+		
+		//Test 4 owned fleets
+		player.getAccount().setOwnedField(dataFields[3]);
+		dataFields[3].setOwner(player);
+		
+		//System.out.println(dataFields[1].getRentsArray()[3]);
+		assertEquals(200, dataFields[0].getRent());
 		
 	}
 
