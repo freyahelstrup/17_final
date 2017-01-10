5f6aa998b4f1fd8dfab04851d7a050ec876be6ce
Gustav Petersen <Rangewaver@gmail.com>
2017-01-10 14:17:58 +0100

    Game does no longer insta close on win

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -69,6 +69,7 @@ public class GameController {
 
 			if (players.length == 1){
 				winnerFound = true;
+				GUIController.getUserButtonPressed(currentPlayer + Messages.getGeneralMessages()[15], "ok");
 			}
 			else{
 				currentPlayer = defineNextPlayer(currentPlayer);
