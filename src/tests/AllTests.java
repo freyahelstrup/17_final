e4336557db3aa3ac0084198edcb628831af6c2c0
Freya Helstrup <freyagh@me.com>
2017-01-12 10:13:04 +0100

    Added Tax_Test

--- src/tests/AllTests.java
+++ src/tests/AllTests.java
@@ -5,7 +5,13 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ Brewery_Test.class, DiceTests.class, Fleet_Test.class, Ownable_Test.class, Street_Test.class })
+@SuiteClasses({ 
+	Brewery_Test.class, 
+	DiceTests.class, 
+	Fleet_Test.class, 
+	Ownable_Test.class, 
+	Street_Test.class, 
+	Tax_Test.class })
 public class AllTests {
 
 }
