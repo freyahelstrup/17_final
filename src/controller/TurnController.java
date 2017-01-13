f3aea63e79d3b461fe9bcab86d2a6c29843d3da3
Theis Gadegaard <theis.gadegaard@gmail.com>
2017-01-13 14:36:24 +0100

    Fixed bug: The owner of a field gets more money than the player landing on his field is able to pay
    Removed an if-statement in TurnController asking whether the owner of
    the field has a negative balance. Instead asks whether the owner of the
    field is the same as the player.
    
    Outcommented the private int jailFreeCounter in class Account. 
    We haven't implemented this.

--- src/controller/TurnController.java
+++ src/controller/TurnController.java
@@ -320,7 +320,7 @@ public class TurnController {
 						Messages.getGeneralMessages()[13]});
 			}
 		// You have to pay rent
-			else if (owner.getAccount().getBalance() >= 0){//pay rent to owner if he is not bankrupt
+			else if (owner != player){ 
 				int rent = 0;
 				if (currentField instanceof Brewery){
 					//when brewery we should multiply dice sum with 4 or 10, depending on the amount of owned brewery from the same owner.
