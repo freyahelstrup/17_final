af94015a32482a6866c1affd8fd1d900421874a0
Freya Helstrup <freyagh@me.com>
2017-01-13 15:20:13 +0100

    Added TC02 and updated buyHouseHotel in TestClass

--- src/tests/TurnController_TestClass.java
+++ src/tests/TurnController_TestClass.java
@@ -197,6 +197,101 @@ public class TurnController_TestClass extends TurnController {
 		}
 	}
 	
+	protected void buyHouseHotel(){
+
+		Street[] ownedStreets = player.getAccount().getBuildableStreets();
+		
+		//we go through owned buildable streets
+		for (Street ownedStreet : player.getAccount().getBuildableStreets()){
+			
+			Color fieldColor = ownedStreet.getColor();
+			
+			//we find number of streets in group and min and max houses
+			int groupAmount = 0;
+			int maxHouses = 0;
+			int minHouses = 5;
+			for (Field boardField : board.getFields()){
+				if (boardField.getColor() == fieldColor && boardField instanceof Street){
+					groupAmount++;
+
+					//we find current min amount of houses in group
+					if(((Street) boardField).getHousesOwned() < minHouses){
+						minHouses = ((Street) boardField).getHousesOwned();
+					}
+					
+					//we find current max amount of houses in group
+					if(((Street) boardField).getHousesOwned() > maxHouses){
+						maxHouses = ((Street) boardField).getHousesOwned();
+					}
+				}
+			}
+			
+			//we find streets in group owned by player
+			int ownedInGroup = 0;
+			for(Ownable ownedField : player.getAccount().getOwnedFields()){
+				if (ownedField.getColor() == fieldColor && ownedField instanceof Street){
+					ownedInGroup++;
+				}
+			}
+			
+			//we find out if player has all streets in group
+			if (ownedInGroup == groupAmount //player owns all in group
+				&& (ownedStreet.getHousesOwned() < maxHouses //field has less houses than other field in group
+					|| (ownedStreet.getHousesOwned() == maxHouses && minHouses == maxHouses))){ //all fields have same number of houses
+				//everything is good
+			}
+			//we remove street from ownedStreets
+			else{
+				Street[] oldArray = ownedStreets;
+				ownedStreets = new Street[oldArray.length-1];
+						
+				int counter = 0;
+				for (Street i : oldArray){
+					if (i != ownedStreet){
+						ownedStreets[counter] = i;
+						counter++;
+					}
+				}
+			}
+		}
+		
+		//we find names of the streets
+		String[] fieldNames = new String[ownedStreets.length];
+
+		for (int i = 0; i < fieldNames.length; i++){
+			fieldNames[i] = Messages.getFieldNames()[ownedStreets[i].getId()-1];
+		}
+
+		//For which street do you want a house/hotel?
+		String userSelection = userChoice[userChoiceCounter];
+
+		Street chosenField = null;
+		for (Field i : board.getFields()){
+			if (Messages.getFieldNames()[i.getId()-1].equals(userSelection)){
+				chosenField = (Street) i;
+			}
+		}
+
+		//Are you sure?
+		String areYouSure = determineUserInput(new String[]{Messages.getGeneralMessages()[23] + chosenField.getHousePrice() + Messages.getGeneralMessages()[24]
+				, Messages.getGeneralMessages()[1], Messages.getGeneralMessages()[2]});
+
+		if (areYouSure.equals(Messages.getGeneralMessages()[1])){
+			chosenField.setHousesOwned(chosenField.getHousesOwned()+1);
+			
+			if (chosenField.getHousesOwned() == 5){
+				GUIController.setHotel(chosenField.getId());
+			}
+			else{
+				GUIController.setHouses(chosenField.getId(),chosenField.getHousesOwned());
+			}
+			
+			player.getAccount().setBalance(player.getAccount().getBalance()-chosenField.getHousePrice());
+			GUIController.setPlayerBalance(player.getName(), player.getAccount().getBalance());
+		}
+		
+	}
+	
 	public void setTestData(int[] data){
 		this.data = data;
 	}
