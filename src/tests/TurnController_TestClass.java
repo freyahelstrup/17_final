02b002dc6e872c43e375f8a74b255cd2f07e41cc
Freya Helstrup <freyahelstrup@me.com>
2017-01-15 18:34:46 +0100

    Changed determineUserInput
    Will now return userChoice

--- src/tests/TurnController_TestClass.java
+++ src/tests/TurnController_TestClass.java
@@ -5,14 +5,12 @@ import java.awt.Color;
 import controller.GUIController;
 import controller.TurnController;
 import entity.Board;
-import entity.Chance;
 import entity.DiceCup;
 import entity.Field;
 import entity.Messages;
 import entity.Ownable;
 import entity.Player;
 import entity.Street;
-import entity.Tax;
 
 public class TurnController_TestClass extends TurnController {
 	private int[] data;
@@ -28,8 +26,7 @@ public class TurnController_TestClass extends TurnController {
 	
 	@Override
 	protected String determineUserInput(String[] input){
-		return input[1]; //We give a 2 length array as the input, this makes us able to go around the GUI
-		//The first string of the user input is usually just the message for the GUI, so we leave that be.
+		return userChoice[userChoiceCounter++]; //We return the current userChoice and then increment the counter
 	}
 	
 	@Override
@@ -61,159 +58,6 @@ public class TurnController_TestClass extends TurnController {
 			movingToPrison = false;
 		}
 		
-	}
-		
-	
-	@Override
-	public void playTurn(){
-		//This used to be in the constructor, but problems arrived when trying to implement
-		//a test class inheriting from this class as it requires the super class constructor to be used,
-		//which in turn prevents us from doing any of our own determined inputs
-		
-		do{
-
-			boolean buyHouseChoice = false;
-			
-			//we go through owned buildable streets
-			for (Street ownedStreet : player.getAccount().getBuildableStreets()){
-				
-				Color fieldColor = ownedStreet.getColor();
-				
-				//we find number of streets in group
-				int groupAmount = 0;
-				for (Field boardField : board.getFields()){
-					if (boardField.getColor() == fieldColor && boardField instanceof Street){
-						groupAmount++;
-					}
-				}
-				
-				//we find streets in group owned by player
-				int ownedInGroup = 0;
-				for(Ownable ownedField : player.getAccount().getOwnedFields()){
-					if (ownedField.getColor() == fieldColor && ownedField instanceof Street){
-						ownedInGroup++;
-					}
-				}
-				
-				//we find out if player has all streets in group
-				if (ownedInGroup == groupAmount){ //player owns all in group
-					buyHouseChoice = true;
-					break;	
-				}
-			}
-
-			//Player can buy houses
-			if (buyHouseChoice == true){
-
-				//Do you want to throw dice or buy houses/hotels
-				player.setChoice(determineUserInput(new String[]{Messages.getGeneralMessages()[11] + player.getName() + Messages.getGeneralMessages()[12],
-					userChoice[userChoiceCounter]}));
-				userChoiceCounter++;
-				
-				//Player wants to buy house or hotel
-				if (player.getChoice().equals(Messages.getGeneralMessages()[21])){
-					buyHouseHotel();
-				}
-			}
-			//Player can only throw dice
-			else{
-				player.setChoice(determineUserInput(new String[]{Messages.getGeneralMessages()[11] + player.getName() + Messages.getGeneralMessages()[12],
-						userChoice[userChoiceCounter]}));
-				userChoiceCounter++;
-			}
-		}
-		while(!player.getChoice().equals(Messages.getGeneralMessages()[7])); //player does not want to throw dice
-
-		movingPiece = false; // A boolean to define whether or not a piece shall move. I saw it necessary to simplify the code.
-
-		// Player is not in prison
-		if (player.getPrisonCount() == 0) {
-			throwDice();
-			movingPiece = true;
-		}
-		// If player is in Prison
-		else if (player.getPrisonCount() > 0) {
-			prisonEscape();
-		}
-
-		if (movingPiece) {
-			movePiece();
-			landOnField();
-			if((currentField instanceof Chance) && (Chance.isMoveCard() == true)){
-				GUIController.removeAllCars(player.getName());
-				currentField = board.getFields()[player.getPiece().getPosition()-1];
-				GUIController.setCar(player.getPiece().getPosition(), player.getName());
-				landOnField();
-			}
-		}
-	}
-	
-	@Override
-	protected void landOnField(){
-		// You landed on
-		if (player.getPiece().getPosition()-1 != 0) { // No need to tell that you landed on start, when the MovePiece says it
-		}
-
-		if (player.getPiece().getPosition()-1 == 30) { // goToPrison field.
-			GUIController.removeAllCars(player.getName());
-			moveToPrison();
-		}
-
-		int playerBalance = player.getAccount().getBalance();
-
-	//Ownable
-		if (currentField instanceof Ownable) {
-			Player owner = ((Ownable) currentField).getOwner();
-			int price = ((Ownable) currentField).getPrice();
-
-		// Do you wish to buy it?
-			if (owner == null && playerBalance >= price) {
-				String playerChoice = determineUserInput(new String[]{
-						Messages.getGeneralMessages()[0] + ((Ownable) currentField).getPrice() + "?"
-						,userChoice[userChoiceCounter]
-								});
-				userChoiceCounter++;
-				
-				player.setChoice(playerChoice);
-
-				if (playerChoice.equals(Messages.getGeneralMessages()[1])) { // User chooses yes
-					GUIController.setFieldOwner(player.getName(), player.getPiece().getPosition());
-				}
-			}
-			// You don't have enough money to buy field
-			else if(owner == null && playerBalance < price){
-			}
-		// You own the field
-			else if (owner == player){
-			}
-		// You have to pay rent
-			else if (owner.getAccount().getBalance() >= 0){//pay rent to owner if he is not bankrupt
-			}
-		}
-
-	//Tax
-		else if( currentField instanceof Tax) {
-			if ( ((Tax) currentField).getTaxRate() > 0) {
-
-			String playerChoice = determineUserInput(new String[]{
-					Messages.getGeneralMessages()[3]
-					,userChoice[userChoiceCounter]});
-			userChoiceCounter++;
-
-			player.setChoice(playerChoice);
-			}
-		}
-	//Chance	
-		else if( currentField instanceof Chance){
-		}
-
-		currentField.landOnField(player);
-		GUIController.setPlayerBalance(player.getName(),player.getAccount().getBalance());
-		if (currentField instanceof Ownable){
-			if (((Ownable) currentField).getOwner() != null){
-				GUIController.setPlayerBalance(((Ownable) currentField).getOwner().getName(),((Ownable) currentField).getOwner().getAccount().getBalance());
-			}
-		}
 	}
 	
 	protected void buyHouseHotel(){
@@ -283,7 +127,8 @@ public class TurnController_TestClass extends TurnController {
 
 		//For which street do you want a house/hotel?
 		String userSelection = userChoice[userChoiceCounter];
-
+		userChoiceCounter++;
+		
 		Street chosenField = null;
 		for (Field i : board.getFields()){
 			if (Messages.getFieldNames()[i.getId()-1].equals(userSelection)){
@@ -308,7 +153,7 @@ public class TurnController_TestClass extends TurnController {
 			player.getAccount().setBalance(player.getAccount().getBalance()-chosenField.getHousePrice());
 			GUIController.setPlayerBalance(player.getName(), player.getAccount().getBalance());
 		}
-		
+
 	}
 	
 	public void setTestData(int[] data){
