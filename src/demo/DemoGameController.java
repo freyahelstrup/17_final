e5e516ca4dd694055bdd631380024b4981512587
Freya Helstrup <freyagh@me.com>
2017-01-16 12:14:31 +0100

    Updated demo

--- src/demo/DemoGameController.java
+++ src/demo/DemoGameController.java
@@ -4,6 +4,7 @@ import controller.GameController;
 import tests.TurnController_TestClass;
 import entity.Board;
 import entity.Player;
+import entity.Chance;
 
 public class DemoGameController extends GameController {
 	int[][] testData = new int[][]{
@@ -16,17 +17,17 @@ public class DemoGameController extends GameController {
 		{1,1}, 	//Roskildevej (leje)
 		{3,1},	//På besøg i fængsel (ryk dobbelt efter terningekast lige med hinanden)
 		
-		{4,1},	//Prøv lykken
+		{4,1},	//Prøv lykken - rykker 3 felter tilbage til Gl. Kongevej
 		{2,1}, 	//DFDS
 		{4,1}, 	//DFDS (leje)
 		
-		{6,2}, 	//ØK
+		{6,5}, 	//ØK
 		{6,4},	//ØK (leje)
 		{4,1},	//Parkering
 		
 		{2,1},	//Carlsberg
 		{2,1},	//Carlsberg (leje) 3*10
-		{1,1},	//Prøv lykken
+		{1,1},	//Prøv lykken - Betal 75 kr for øl
 		{1,1},	//Grønningen
 		{1,1}, 	//Fængsel
 		
@@ -36,12 +37,13 @@ public class DemoGameController extends GameController {
 		
 		{1,1}, 	//Spiller 1 slår ud
 		{1,2},	//DFDS
-		{1,1},	//Start
-		{2,1}, 	//Hvidovrevej
+		{1,2},	//Rødovrevej
 		{1,2}, 	//Spiller 3 slår ikke ud
 		
+		{1,1}, 	//Prøv lykken - Matadorlegatet
 		{1,3}, 	//Ikke vigtigt slag
-		{1,3}, 	//Ikke vigtigt slag
+		{1,1}, 	//Hvidovre
+		{1,6},  //Køb hus - Fængsel
 		{1,3}	//Spiller 3 slår ikke ud og er tvunget til at betale 50 kr.
 		
 		};
@@ -54,6 +56,8 @@ public class DemoGameController extends GameController {
 	public void playGame(){
 		boolean winnerFound = false;
 		Player currentPlayer;
+		
+		((Chance) board.getFields()[2]).setDeck(new int[]{6, 11, 17});
 
 		//first player is player 1
 		currentPlayer = players[0];
